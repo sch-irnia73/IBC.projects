3303626e0387435dea7664a1d5929eccbabfe555
Ira <i.shchetinina@innopolis.univercity>
2021-08-04 16:06:32 +0300

    Initial commit

---
+++ IBC.project1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main(void) {
+
+    int a, d;
+    d = 0;
+    cout << "array size: ";
+    cin >> a;
+    vector <int> n(a);
+
+    for (int i = 0; i < a; i++) {
+        cin >> n[i];
+    }
+
+    for (int i = 0; i < a; i++) {
+        for (int c = 0; c < a-1; c++) {
+            if (n[c] > n[c + 1]) {
+                d = n[c];
+                n[c] = n[c + 1];
+                n[c + 1] = d;
+            }
+        }
+    }
+    for (int i = 0; i < a; i++) {
+        cout << n[i] << " ";
+    }
+    return 0;
+}
